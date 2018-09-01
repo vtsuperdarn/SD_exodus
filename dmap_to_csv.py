7e8349560dd3c381d8962235241ad7f42c34483a
MuhammadVT <mamatripik@gmail.com>
2018-09-01 18:55:40 -0400

    some clean up, add print statements

--- dmap_to_csv.py
+++ dmap_to_csv.py
@@ -11,9 +11,6 @@ import os
 import string
 import numpy as np
 
-# Set the logging level
-logging.basicConfig(level=logging.INFO)
-
 def fetch_concat(ctr_date, localdirfmt, localdict, tmpdir,
                  fnamefmt, remove_extra_file=True):
 
@@ -26,17 +23,15 @@ def fetch_concat(ctr_date, localdirfmt, localdict, tmpdir,
         a full day for which data are to be read. 
     localdirfmt : str
         string defining the local directory structure
-        (eg "{ftype}/{year}/{month}/{day}/")
     localdict : dict
-        Contains keys for non-time related information in remotedirfmt and
-        fnamefmt (eg remotedict={'ftype':'fitex','radar':'sas','channel':'a'})  
+        Contains keys for non-time related information
+        fnamefmt (eg localdict={'ftype':'fitacf','radar':'sas','channel':'a'})  
     tmpdir : str
-        Temporary directory in which to store uncompressed files (must end with
-        a "/").	
+        Temporary directory in which to store uncompressed files.	
     fnamefmt : str or list
         Optional string or list of file name formats
         (eg fnamefmt = ['{date}.{hour}......{radar}.{channel}.{ftype}', \
-            '{date}.C0.{radar}.{ftype}'] 
+            '{date}.{channel}.{radar}.{ftype}'] 
         or fnamefmt = '{date}.{hour}......{radar}.{ftype}')	
     remove_extra_file : bool
         If set to True, files other than the concatenated file will be removed
@@ -67,7 +62,7 @@ def fetch_concat(ctr_date, localdirfmt, localdict, tmpdir,
     # check if we have found files
     if len(file_list) != 0:
         # concatenate the files into a single file
-        logging.info('Concatenating all the files in to one')
+        print('Concatenating all the files in to one')
 
         # choose a temp file name with time span info for cacheing
         if (channel is None) or (channel == "."):
@@ -83,17 +78,17 @@ def fetch_concat(ctr_date, localdirfmt, localdict, tmpdir,
                        etime.strftime("%Y%m%d"),
                        etime.strftime("%H%M%S"),
                        radcode, channel, ftype)
-        logging.info('cat ' + string.join(file_list) + ' > ' + fname)
+        logging.debug('cat ' + string.join(file_list) + ' > ' + fname)
         os.system('cat ' + string.join(file_list) + ' > ' + fname)
 
         # remove the unneeded files from the tmpdir
         if remove_extra_file:
+            print("removing unneeded files")
             for fn in file_list:
-                logging.info('rm ' + fn)
+                logging.debug('rm ' + fn)
                 os.system('rm ' + fn)
                 os.system('rm ' + fn+".bz2")
                 #os.system('rm ' + fn+".gz")
-                logging.info("removed unneeded files")
     else:
         fname = None
         
@@ -102,14 +97,16 @@ def fetch_concat(ctr_date, localdirfmt, localdict, tmpdir,
 # run the code
 def main():
 
+    # Set the logging level
+    logging.getLogger().setLevel(logging.WARNING)
+
     # input parameters
     ctr_date = dt.datetime(2012,12,31)
-    #rad = "fhe"
-    rad = "ade"
-    channel = None
+    rad = "fhe"
+    #rad = "ade"
+    channel = "."
     ftype = "fitacf"
-    scr = "local"
-    remove_extra_file = False 
+    remove_extra_file = True 
     localdirfmt = "/sd-data/{year}/{ftype}/{radar}/"
     localdict = {"ftype" : ftype, "radar" : rad, "channel" : channel}
     tmpdir = "./data/tmp/"
